017af61d1c45d8f0c2d9d13966ec9c01a50b84ce
jackyjin <jackyjin@qq.com>
2020-10-28 14:34:40 +0800

    first

--- src/main/java/com/jxd/studentmanager/mapper/IStudentMapper.java
+++ src/main/java/com/jxd/studentmanager/mapper/IStudentMapper.java
@@ -3,5 +3,8 @@ package com.jxd.studentmanager.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.jxd.studentmanager.model.Student;
 
+import java.util.Map;
+
 public interface IStudentMapper extends BaseMapper<Student> {
+    Map<String,Object> selectDetails(int sid);
 }
---
+++ src/main/test/com.jxd.studentmanager/testmapper/TestStudentMapper.java
@@ -0,0 +1,32 @@
+package com.jxd.studentmanager.testmapper;
+
+import com.jxd.studentmanager.StudentManagerApplication;
+import com.jxd.studentmanager.mapper.IStudentMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ClassName TestStudentMapper
+ * @Description TODO
+ * @Author jinkaiyan
+ * @Date 2020/10/28
+ * @Version 1.0
+ */
+@RunWith(SpringRunner.class)//用谁作为启动器去加载所有的底层配置
+@SpringBootTest(classes = StudentManagerApplication.class)
+public class TestStudentMapper {
+    @Autowired
+    private IStudentMapper studentMapper;
+
+    @Test
+    public void testSelectDetails() {
+        Map<String, Object> map = studentMapper.selectDetails(1);
+        System.out.println(map.toString());
+    }
+}
