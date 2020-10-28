14fe98c4779dca0e9a25e59f1890c1d23e6ae708
znn <2519340942@qq.com>
2020-10-28 14:04:43 +0800

    123

---
+++ src/main/java/com/jxd/studentmanager/controller/StudentScoreController.java
@@ -0,0 +1,14 @@
+package com.jxd.studentmanager.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @ClassName StudentScoreController
+ * @Description TODO
+ * @Author Zheng NaNa
+ * @Date 2020/10/28
+ * @Version 1.0
+ */
+@Controller
+public class StudentScoreController {
+}
---
+++ src/main/java/com/jxd/studentmanager/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.jxd.studentmanager.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @ClassName UserController
+ * @Description TODO
+ * @Author Zheng NaNa
+ * @Date 2020/10/28
+ * @Version 1.0
+ */
+@Controller
+public class UserController {
+    public String login(){
+        return "";
+    }
+}
