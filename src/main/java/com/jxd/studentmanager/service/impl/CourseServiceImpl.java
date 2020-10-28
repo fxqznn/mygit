e9c7f7eafae76cde6d56794ceac7c4ed0907135e
jackyjin <jackyjin@qq.com>
2020-10-28 17:57:47 +0800

    CourseMapper

--- src/main/java/com/jxd/studentmanager/service/impl/CourseServiceImpl.java
+++ src/main/java/com/jxd/studentmanager/service/impl/CourseServiceImpl.java
@@ -1,6 +1,7 @@
 package com.jxd.studentmanager.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jxd.studentmanager.mapper.ICourseMapper;
 import com.jxd.studentmanager.model.Course;
 import com.jxd.studentmanager.service.ICourseService;
 import org.springframework.stereotype.Service;
