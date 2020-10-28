1b85ac8fb5832cc62d12cfb415bec808f0477b03
fxq <fxq@qq.com>
2020-10-28 13:53:14 +0800

    fxq fxq@qq.com

---
+++ src/main/java/com/jxd/studentmanager/mapper/ICourseMapper.java
@@ -0,0 +1,14 @@
+package com.jxd.studentmanager.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jxd.studentmanager.model.Course;
+
+/**
+ * @ClassName ICourseMapper
+ * @Description TODO
+ * @Author fengxueqin
+ * @Date 2020/10/28
+ * @Version 1.0
+ */
+public interface ICourseMapper extends BaseMapper<Course> {
+}
