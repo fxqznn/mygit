16887b603a929ea5c18a2949d224b7e54fbcad35
znn <2519340942@qq.com>
2020-10-29 16:41:49 +0800

    123

--- src/main/java/com/jxd/studentmanager/controller/CourseController.java
+++ src/main/java/com/jxd/studentmanager/controller/CourseController.java
@@ -11,4 +11,89 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class CourseController {
+    @Autowired
+    ICourseService courseService;
+
+    @Autowired
+    ITermCourseService termCourseService;
+
+    /**
+     * 查询课程信息，返回列表中包含tid
+     * @param tid
+     * @return
+     */
+    @RequestMapping("/getCourses")
+    @ResponseBody
+    public List<Map<String,Object>> getCoursesBytid(int tid){
+        return courseService.selectCoursesByTid(tid);
+    }
+
+    /**
+     * 老师往课程表中批量插入课程
+     * @param courses
+     * @return
+     */
+    @RequestMapping("/addCourseBatch")
+    @ResponseBody
+    public String addCourseBatch(List<Course> courses){
+        boolean flag_tc = courseService.saveBatch(courses);
+        if (flag_tc){
+            return "success";
+        } else {
+            return "error";
+        }
+    }
+
+    /**
+     * 根据cid和tid添加学期课程，如果存在cid和tid与将要插入的同时相同时
+     * 则不添加
+     * @param cid
+     * @param tid
+     * @return
+     */
+    @RequestMapping(value = "addTermCourse" ,produces = "html/text;charset=utf-8")
+    @ResponseBody
+    public String addTermCourse(int cid,int tid){
+        List<TermCourse> termCourses = termCourseService.list();
+        boolean flag = false;
+        for (TermCourse termCourse: termCourses){
+            if (termCourse.getTid() == tid && termCourse.getCid() == cid){
+                flag = false;
+                break;
+            } else {
+                flag = true;
+            }
+        }
+        if (flag){
+            TermCourse termCourse = new TermCourse();
+            termCourse.setTid(tid);
+            termCourse.setCid(cid);
+            if (termCourseService.save(termCourse)){
+                return "课程添加成功";
+            } else {
+                return "课程已存在";
+            }
+        }else {
+            return "课程添加失败";
+        }
+    }
+
+    @RequestMapping(value = "updateCourseBatch",produces = "html/text;charset=utf-8")
+    @ResponseBody
+    public String updateCourseBatch(List<Course> courses){
+        boolean flag = false;
+        for(Course course:courses){
+            UpdateWrapper<Course> updateWrapper = new UpdateWrapper<>();
+            updateWrapper.eq("cid",course.getCid());
+            flag = courseService.update(course,updateWrapper);
+            if (!flag){
+                return "修改失败";
+            }
+        }
+        if (!flag){
+            return "修改失败";
+        } else {
+            return "修改成功";
+        }
+    }
 }
--- src/main/java/com/jxd/studentmanager/service/ICourseService.java
+++ src/main/java/com/jxd/studentmanager/service/ICourseService.java
@@ -4,4 +4,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.jxd.studentmanager.model.Course;
 
 public interface ICourseService extends IService<Course> {
+
+    List<Map<String,Object>> selectCoursesByTid(int tid);
 }
