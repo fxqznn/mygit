f9e30c9840d857f46d77f2a7680fcd073003c02e
jackyjin <jackyjin@qq.com>
2020-10-29 09:32:08 +0800

    Merge remote-tracking branch 'origin/main' into main

--- src/main/java/com/jxd/studentmanager/controller/StudentController.java
+++ src/main/java/com/jxd/studentmanager/controller/StudentController.java
@@ -1,6 +1,11 @@
 package com.jxd.studentmanager.controller;
 
+import com.jxd.studentmanager.model.Student;
+import com.jxd.studentmanager.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @ClassName StudentController
@@ -11,4 +16,25 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class StudentController {
+
+    @Autowired
+    private IStudentService studentService;
+
+    @RequestMapping("/getSelf")
+    @ResponseBody
+    public Student getSelf(int sid) {
+        return studentService.getById(sid);
+    }
+
+    @RequestMapping("/updateSelf")
+    @ResponseBody
+    public String updateSelf(Student student) {
+        boolean flag = studentService.updateById(student);
+        if (flag) {
+            return "修改信息成功";
+        } else {
+            return "修改信息失败";
+        }
+    }
+
 }
