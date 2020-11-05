104e0b723578d6c44dc5ba1ed41e630191f358c4
wk <1037128119@qq.com>
2020-11-05 15:53:57 +0800

    wk

--- src/main/java/com/jxd/studentmanager/controller/StudentController.java
+++ src/main/java/com/jxd/studentmanager/controller/StudentController.java
@@ -37,6 +37,11 @@ public class StudentController {
     @Autowired
     private IStudentScoreService studentScoreService;
 
+    @RequestMapping("/getStudent")
+    @ResponseBody
+    public Student getStudent(int eid) {return studentService.getStudentByEid(eid)}
+
+
     @RequestMapping("/getSelf")
     @ResponseBody
     public Student getSelf(int sid) {
